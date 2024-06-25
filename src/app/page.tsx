1a81b346c200638dd3428ffbd3a644193a70a58d
JayvonK <jaykhuth@gmail.com>
2024-06-25 16:08:03 -0700

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import InputComponent from "@/components/InputComponent";
 import { IForm } from "@/interfaces/Interfaces";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
@@ -11,7 +11,7 @@ export default function Home() {
     firstName: "",
     lastName: "",
     email: "",
-    dateOfBirth: undefined,
+    dateOfBirth: "",
     address: "",
     phone: "",
     password: "",
@@ -22,23 +22,114 @@ export default function Home() {
 
   const [submitted, setSubmitted] = useState<boolean>(false);
 
+  const [canSubmit, setCanSubmit] = useState<boolean>(false);
+
   const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const [showPasswordTwo, setShowPasswordTwo] = useState<boolean>(false);
+
+  const [presentDay, setPresentDay] = useState<string>("")
 
   const passwordMatch = form.password === form.confirmPassword;
 
-  const presentDay = new Date().toISOString().split('T')[0];
-
   const [count, setCount] = useState<number>(3);
+
+  const [firstNameError, setFirstNameError] = useState<boolean>(false);
+  const [lastNameError, setLastNameError] = useState<boolean>(false);
+  const [dateError, setDateError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [phoneNumberError, setPhoneNumberError] = useState<boolean>(false);
+  const [passwordError, setPasswordError] = useState<boolean>(false);
 
   const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value })
+
+    switch (e.target.name) {
+      case "email":
+        e.target.value.includes("@") ? setEmailError(false) : setEmailError(true);
+        break;
+
+      case "dateOfBirth":
+        new Date(presentDay) < new Date(e.target.value) ? setDateError(true) : setDateError(false);
+        break;
+
+      case "phone":
+        if (e.target.value.trim() === "") {
+          setPhoneNumberError(false);
+        } else {
+          const correct = /\([0-9]{3}\)-[0-9]{3}-[0-9]{4}/.test(e.target.value)
+
+          correct ? setPhoneNumberError(false) : setPhoneNumberError(true);
+        }
+        break;
+
+      case "firstName":
+        e.target.value.trim() === "" ? setFirstNameError(true) : setFirstNameError(false);
+        break;
+
+      case "lastName":
+        e.target.value.trim() === "" ? setLastNameError(true) : setLastNameError(false);
+        break;
+
+      case "password":
+        if (e.target.value.trim() === "" || e.target.value.length < 15) {
+          setPasswordError(true);
+        } else {
+          const included = /^(?=.*[?!@#$%^&*])(?=.*[A-Z])(?=.*[0-9]).*$/.test(e.target.value);
+          const notSpecial = /[^a-zA-Z0-9?!@#$%^&*]/.test(e.target.value);
+          if (included && !notSpecial) {
+            setPasswordError(false);
+          } else if (!included || notSpecial) {
+            setPasswordError(true);
+          }
+        }
+    }
+
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setSubmitted(true);
-    afterSubmit();
+  const checkInputs = () => {
+    let count = 0;
+    if (form.firstName.trim() === "") {
+      setFirstNameError(true);
+      count++;
+    }
+
+    if (form.lastName.trim() === "") {
+      setLastNameError(true);
+      count++;
+    }
+
+    if (!form.email.includes("@")) {
+      setEmailError(true);
+      count++;
+    }
+
+    if (form.password.trim() === "") {
+      setPasswordError(true);
+      count++;
+    }
+
+    if (form.dateOfBirth === "") {
+      setDateError(true);
+      count++;
+    }
+
+    return count === 0 ? true : false;
   }
+
+  const handleSubmit = () => {
+    
+    if (!(firstNameError && lastNameError && emailError && passwordError && dateError) && checkInputs()) {
+      setSubmitted(true);
+      afterSubmit();
+    }
+
+    // if (canSubmit) {
+    //   setSubmitted(true);
+    //   afterSubmit();
+    // }
+  }
+
 
   const afterSubmit = () => {
     setTimeout(() => {
@@ -52,62 +143,70 @@ export default function Home() {
     }, 2000)
 
     setTimeout(() => {
-      
+
       setCount(0)
       setSubmitted(false);
       setCount(3);
     }, 3000)
   }
 
+  const handleShowPasswordOne = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const handleShowPassowrdTwo = () => {
+    setShowPasswordTwo(!showPasswordTwo)
+  }
+
   // const included = /^(?=.*[?!@#$%^&*])(?=.*[A-Z])(?=.*[0-9]).*$/.test(form.password);
   // const notSpecial = /[^a-zA-Z0-9?!@#$%^&*]/.test(form.password);
+
+  useEffect(() => {
+    setPresentDay(new Date().toISOString().split('T')[0]);
+  }, [])
 
   return (
     <main className="min-h-screen w-full bg-[#23527C]">
       <div className="flex items-center flex-col">
         <img src="/WA-Logo.png" alt="William's Act Logo" />
-        <div className="bg-white px-6 py-4 min-w-72">
+        <div className="bg-white px-6 py-4 min-w-72 max-w-[538px] mb-12">
+
           <h1 className="text-center text-[34px] text-black mb-6 robotoCondensed font-light ">USER <strong className="font-bold">SIGN UP</strong></h1>
 
-          {!submitted ? <form onSubmit={handleSubmit} className="openSans font-semibold">
-            <div className="grid sm:grid-cols-2 gap-6 ">
-              <InputComponent for={"First Name"} label={"First Name:"} required={true} onChange={handleFormChange} value={form.firstName} name="firstName" max={100} type={"text"} placeHolder={"First Name"} />
+          {
+            !submitted ? <form className="openSans font-semibold">
+              <div className="grid sm:grid-cols-2 gap-6 ">
+                <InputComponent for={"First Name"} label={"First Name:"} required={true} onChange={handleFormChange} value={form.firstName} name="firstName" max={100} type={"text"} placeHolder={"First Name"} firstNameError={firstNameError} />
 
-              <InputComponent for={"Last Name"} label={"Last Name:"} required={true} onChange={handleFormChange} value={form.lastName} name="lastName" max={100} type={"text"} placeHolder={"Last Name"} />
+                <InputComponent for={"Last Name"} label={"Last Name:"} required={true} onChange={handleFormChange} value={form.lastName} name="lastName" max={100} type={"text"} placeHolder={"Last Name"} lastNameError={lastNameError} />
 
-              <InputComponent for={"Email"} label={"Email:"} required={true} onChange={handleFormChange} value={form.email} name="email" type={"email"} placeHolder={"Email"} />
+                <InputComponent for={"Email"} label={"Email:"} required={true} onChange={handleFormChange} value={form.email} name="email" type={"email"} placeHolder={"Email"} emailError={emailError} />
 
-              <InputComponent for={"Date Of Birth"} label={"Date Of Birth:"} required={true} onChange={handleFormChange} value={form.dateOfBirth} name="dateOfBirth" type={"date"} placeHolder={"Date Of Birth"} maxDate={presentDay} />
+                <InputComponent for={"Date Of Birth"} label={"Date Of Birth:"} required={true} onChange={handleFormChange} value={form.dateOfBirth} name="dateOfBirth" type={"date"} placeHolder={"Date Of Birth"} maxDate={presentDay} dateError={dateError} />
 
-              <InputComponent for={"Address"} label={"Address:"} required={false} onChange={handleFormChange} value={form.address} name="address" type={"text"} max={100} placeHolder={"Address"} />
+                <InputComponent for={"Address"} label={"Address:"} required={false} onChange={handleFormChange} value={form.address} name="address" type={"text"} max={100} placeHolder={"Address"} />
 
-              <InputComponent for={"Phone Number"} label={"Phone Number:"} required={form.phone.trim() !== ""} onChange={handleFormChange} value={form.phone} name="phone" type={"tel"} pattern="\([0-9]{3}\)-[0-9]{3}-[0-9]{4}" placeHolder={"(123)-456-7890"} title={"Phone Number must be in this format: (123)-456-7890"} />
+                <InputComponent for={"Phone Number"} label={"Phone Number:"} required={form.phone.trim() !== ""} onChange={handleFormChange} value={form.phone} name="phone" type={"tel"} pattern="\([0-9]{3}\)-[0-9]{3}-[0-9]{4}" placeHolder={"(123)-456-7890"} title={"Phone Number must be in this format: (123)-456-7890"} phoneError={phoneNumberError} />
 
-              <InputComponent for={"Password"} label={"Password:"} required={true} onChange={handleFormChange} value={form.password} name="password" type={showPassword ? "text" : "password"} placeHolder={"Password"} min={15} pattern="(?=.*[?!@#$%^&*])(?=.*[A-Z])(?=.*[\d])[a-zA-Z0-9?!@#$%^&*]+" title={"Password must have 15 characters minimum, have 1 uppercase, 1 lowercase and 1 of these special characters ? ! @ # $ % ^ & * "} />
+                <InputComponent for={"Password"} label={"Password:"} required={true} onChange={handleFormChange} value={form.password} name="password" type={showPassword ? "text" : "password"} placeHolder={"Password"} min={15} pattern="(?=.*[?!@#$%^&*])(?=.*[A-Z])(?=.*[\d])[a-zA-Z0-9?!@#$%^&*]+" title={"Password must have 15 characters minimum, have 1 uppercase, 1 lowercase and 1 of these special characters ? ! @ # $ % ^ & * "} showPasswordOne={showPassword} handleShowPasswordOne={handleShowPasswordOne} passwordError={passwordError} />
 
-              <InputComponent for={"Confirm Password"} label={"Confirm Password:"} required={true} onChange={handleFormChange} value={form.confirmPassword} name="confirmPassword" type={showPassword ? "text" : "password"} placeHolder={"Re-Type Your Password"} error={!passwordMatch} min={15} />
-            </div>
+                <InputComponent for={"Confirm Password"} label={"Confirm Password:"} required={true} onChange={handleFormChange} value={form.confirmPassword} name="confirmPassword" type={showPasswordTwo ? "text" : "password"} placeHolder={"Re-Type Your Password"} passwordMatchError={!passwordMatch} min={15} showPasswordTwo={showPasswordTwo} handleShowPassowrdTwo={handleShowPassowrdTwo} />
 
-            <div className="flex justify-center">
+              </div>
 
-              <img className="hover:cursor-pointer" src={ showPassword ? "/eye.svg" : "/eye-slash.svg"} alt="eyeball" onClick={() => setShowPassword(!showPassword)}/>
+              <div className="flex justify-center mt-6 w-full">
+                <button type="button" className="bg-[#DD8A3E] hover:brightness-90 p-4 w-full text-white text-sm font-bold tracking-wide" onClick={handleSubmit}>SIGN UP</button>
+              </div>
+            </form>
+              :
+              <div className="flex justify-center items-center flex-col">
+                <img src="/check-fat-fill.svg" alt="green checkmark" className="aspect-square w-14" />
+                <h3 className="openSans font-bold text-center mt-6 sm:mx-32 mx-16">Account Creation <br />Successful!</h3>
 
-            </div>
+                <h3 className="openSans font-bold text-center mt-3 mb-6 sm:mx-32 mx-16">Redirecting in {count}</h3>
 
-            <div className="flex justify-center mt-6 w-full">
-              <button type="submit" className="bg-[#DD8A3E] hover:brightness-90 p-4 w-full text-white text-sm font-bold tracking-wide">SIGN UP</button>
-            </div>
-          </form>
-            :
-            <div className="flex justify-center items-center flex-col">
-              <img src="/check-fat-fill.svg" alt="green checkmark" className="aspect-square w-14" />
-              <h3 className="openSans font-bold text-center mt-6 sm:mx-32 mx-16">Account Creation <br />Successful!</h3>
-
-              <h3 className="openSans font-bold text-center mt-3 mb-6 sm:mx-32 mx-16">Redirecting in {count}</h3>
-
-            </div>}
-
-
+              </div>
+          }
         </div>
       </div>
     </main>
--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
-const InputComponent = (props: {for: string, max?: number, label: string, required: boolean, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, value: string | number, name: string, type: string, pattern?: string, placeHolder: string, error?: boolean, min?: number, maxDate?: string, title? : string}) => {
+const InputComponent = (props: { for: string, max?: number, label: string, required: boolean, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, value?: string | number, name: string, type: string, pattern?: string, placeHolder: string, passwordMatchError?: boolean, min?: number, maxDate?: string, title?: string, showPasswordOne?: boolean, showPasswordTwo?: boolean, handleShowPasswordOne?: () => void, handleShowPassowrdTwo?: () => void, emailError?: boolean, firstNameError?: boolean, lastNameError?: boolean, dateError?: boolean, passwordError?: boolean, phoneError?: boolean }) => {
   return (
-    <div className='flex flex-col justify-end relative'>
-        {props.label === "Date Of Birth:" && <label className='text-sm text-gray-400 absolute -top-5'>(Birthday)</label>}
-        {props.label !== "Address:" && props.label !== "Phone Number:" && <p className='text-red-600 absolute top-0 right-1'>*</p>}
-        {props.error && <p className='text-red-500 text-xs absolute -bottom-1'>Passwords Do Not Match</p>}
-        <input className="text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12" type={props.type} maxLength={props.max} required={props.required} onChange={props.onChange} value={props.value} name={props.name} pattern={props.pattern} placeholder={props.placeHolder} minLength={props.min} max={props.maxDate} title={props.title}/>
+    <div className='flex flex-col relative'>
+      {props.label === "Date Of Birth:" && <label className='text-sm text-gray-400 absolute -top-5'>(Birthday)</label>}
+
+      {props.label !== "Address:" && props.label !== "Phone Number:" && <p className='text-red-600 absolute top-0 right-1'>*</p>}
+
+
+
+      {(props.label === "Password:" || props.label === "Confirm Password:") && <img className="hover:cursor-pointer absolute top-3 right-3 aspect-square w-6" src={props.label === "Password:" ? props.showPasswordOne ? "/eye.svg" : "/eye-slash.svg" : props.showPasswordTwo ? "/eye.svg" : "/eye-slash.svg"} alt="eyeball" onClick={props.label === "Password:" ? props.handleShowPasswordOne : props.handleShowPassowrdTwo} />}
+
+      <input className={`text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12 ${props.label === "Password:" || props.label === "Confirm Password:" ? "pr-10" : ""}`} type={props.type} maxLength={props.max} required={props.required} onChange={props.onChange} value={props.value} name={props.name} pattern={props.pattern} placeholder={props.placeHolder} minLength={props.min} max={props.maxDate} title={props.title} />
+      {props.passwordError && <p className='text-red-500 text-xs -mt-2'>Password must be 15 characters minimum, have 1 uppercase, 1 lowercase and 1 of these special characters ? ! @ # $ % ^ & *. NO OTHER SPECIAL CHARACTERS</p>}
+
+      {props.firstNameError && <p className='text-red-500 text-xs -mt-2'>Cannot Be Empty</p>}
+
+      {props.lastNameError && <p className='text-red-500 text-xs -mt-2'>Cannot Be Empty</p>}
+
+      {props.emailError && <p className='text-red-500 text-xs -mt-2'>Must include @ symbol</p>}
+
+      {props.dateError && <p className='text-red-500 text-xs -mt-2'>Cannot be empty or a future date</p>}
+
+      {props.phoneError && <p className='text-red-500 text-xs -mt-2'>Must be in the format: (xxx)-xxx-xxxx</p>}
+
+      {props.passwordMatchError && <p className='text-red-500 text-xs -mt-2'>Passwords Do Not Match</p>}
     </div>
   )
 }
